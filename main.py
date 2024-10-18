69b7cd1b1af56b4e95d740c8f07a2bacb0e05523
UnTenseUnJury <untenseunjury@gmail.com>
2024-10-18 21:29:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-gfrom flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import hashlib
 import os
